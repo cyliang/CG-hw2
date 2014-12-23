5d61da6ccd59abbc711fe5139593f65fa8dc088e
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2014-12-23 13:40:36 +0800

    finish displacement map

--- Examples/GeometryShader/shader.vert
+++
@@ -1,8 +0,0 @@
-//CIRL GPU Vertex Program: Derek Anderson and Robert Luke
-// very simple vertex shader
-
-void main()
-{	
-	//Transform the vertex (ModelViewProj matrix)
-	gl_Position = ftransform();
-}
---
+++ Examples/PhongShading/display.cpp
@@ -0,0 +1,42 @@
+#include "../SharedCode/header.h"
+#include <iostream>
+using namespace std;
+
+void display() {
+	extern ViewingLoader *viewPtr;
+	extern SceneLoader *scenePtr;
+	extern LightLoader *lightPtr;
+	extern GLhandleARB MyShader;
+	extern float coef;
+
+	viewPtr->setViewing();
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(MyShader);
+
+	GLint locationColor = glGetUniformLocation(MyShader, "colorTexture");
+	GLint locationHeight = glGetUniformLocation(MyShader, "heightTexture");
+	GLint locationCoef = glGetUniformLocation(MyShader, "coef");
+	if (locationColor == -1 || locationHeight == -1 || locationCoef == -1)
+		cerr << "Cant find texture name: coef, colorTexture or heightTexture." << endl;
+	else {
+		glUniform1i(locationColor, 0);
+		glUniform1i(locationHeight, 1);
+		glUniform1f(locationCoef, coef);
+	}
+
+	lightPtr->setLight();
+	scenePtr->displayScene();
+
+	glutSwapBuffers();
+	glFlush();
+}
+
+void redisplay() {
+	glutPostRedisplay();
+}
